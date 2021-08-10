1372ae94933afcb0391fbab09ed9dfdf4ed45696
OrangeBurrito <mali.kewl.pool@gmail.com>
2021-08-10 17:17:21 +0700

    added createPageSelector

--- src/lib/pageSelector.js
+++ src/lib/pageSelector.js
@@ -1,3 +1,31 @@
-function createPageSelector() {
-	
+import {writable} from 'svelte/store'
+
+export function createPageSelector(total, limit) {
+	const state = {
+		total,
+		limit,
+		page: 1
+	}
+
+	const store = writable(state)
+
+	function previousPage() {
+		store.update(s => {
+			s.page = s.page > 1 ? s.page - 1 : s.page
+			return s
+		})
+	}
+
+	function nextPage() {
+		store.update(s => {
+			s.page = s.page < s.total ? s.page + 1 : s.page
+			return s
+		})
+	}
+
+	return {
+		pageSelector: store,
+		nextPage,
+		previousPage
+	}
 }
